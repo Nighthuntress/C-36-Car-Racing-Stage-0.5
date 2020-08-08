269286a821c538ddd86bf10aa374ee5a023fd2fe
Nighthuntress <tashidorjee@gmail.com>
2020-08-08 21:38:21 +0530

    Created play state

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,37 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountref = await database.ref('playerCount').once("value");
+      if(playerCountref.exists()){
+        playerCount = playerCountref.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+
+    if(allPlayers!==undefined){
+       var displayPosition = 130;
+       for(var plr in allPlayers){
+         displayPosition = displayPosition+20;
+         textSize(15);
+         text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,displayPosition);
+       }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index!==null){
+      player.distance = player.distance+50;
+      player.update();
     }
   }
 }
